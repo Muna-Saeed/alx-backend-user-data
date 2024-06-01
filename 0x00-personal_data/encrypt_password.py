535ae38e8a092bce93c13ea33baa4802c7a3059f
Muna Saeed <munasaeed6367@gmail.com>
2024-06-01 07:07:59 -0700

    done

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -18,6 +18,7 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """
     Validate if a plaintext password matches a hashed password.
