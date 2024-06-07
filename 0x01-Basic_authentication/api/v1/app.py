eae29fc63f383f220fc320dfe77af87f6141005b
Muna-Saeed <munasaeed6367@gmail.com>
2024-06-07 16:13:29 -0700

    done

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,7 +12,8 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
+auth = os.environ.get('AUTH_TYPE', None)
+exclude = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
 if AUTH_TYPE:
     from api.v1.auth.auth import Auth
     auth = Auth()
