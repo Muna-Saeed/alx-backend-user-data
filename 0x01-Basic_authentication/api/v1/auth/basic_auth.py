08918dbc66b05c5df90c328fd1320bbeef9bc118
Muna-Saeed <munasaeed6367@gmail.com>
2024-06-08 09:49:49 -0700

    done

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" BasicAuth module for the API
+"""
+from api.v1.auth.auth import Auth
+
+class BasicAuth(Auth):
+    """ BasicAuth class for API authentication """
+    pass
