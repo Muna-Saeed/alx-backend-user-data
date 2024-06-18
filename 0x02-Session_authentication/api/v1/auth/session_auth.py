b97cc7082dc7a608c33644d5370d747edb52966b
Muna-Saeed <124080410+Muna-Saeed@users.noreply.github.com>
2024-06-18 18:50:00 +0300

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,8 @@
-# api/v1/auth/session_auth.py
+#!/usr/bin/env python3
+""" creating a new authentication mechanism """
 
 from api.v1.auth.auth import Auth
+
 
 class SessionAuth(Auth):
     """Session authentication class that inherits from Auth"""
