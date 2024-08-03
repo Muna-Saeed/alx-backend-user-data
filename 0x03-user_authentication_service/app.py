0d4709e4a27c62cbbc5efdaa6c5596346325f7b1
Muna Saeed <munasaeed6367@gmail.com>
2024-08-04 00:03:50 +0300

    done

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -58,5 +58,35 @@ def logout():
         abort(403)
 
 
+@app.route('/profile', methods=['GET'])
+def profile():
+    """Return the user's profile based on session_id."""
+    session_id = request.cookies.get('session_id')
+
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            return jsonify({"email": user.email})
+
+    abort(403)
+
+
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token():
+    """Handle POST request to /reset_password to generate a reset token."""
+    email = request.form.get('email')
+
+    if email is None:
+        abort(400, description="Missing email parameter")
+
+    try:
+        # Generate reset token
+        reset_token = auth.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        # If email is not registered
+        abort(403, description="Email not registered")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -99,3 +99,14 @@ class Auth:
         """Destroy the session of the user with the given user_id."""
         # Update the user's session ID to None
         self._db.update_user_session(user_id, None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generates a reset password token
+         for the user with the given email."""
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user_reset_token(email, reset_token)
+            return reset_token
+        except NoResultFound:
+            raise ValueError("User not found")
