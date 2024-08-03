fe0f58940f85e00256543b2194778757e419c408
Muna Saeed <munasaeed6367@gmail.com>
2024-08-03 20:29:00 +0300

    done

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -59,3 +59,12 @@ class Auth:
             # Add the user to the database
             user = self._db.add_user(email, hashed_password)
             return user
+
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validates login credentials."""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+        except NoResultFound:
+            return False
