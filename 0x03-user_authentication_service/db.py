786c3c957120d06f3b0787cb5177594fd804481c
Muna-Saeed <124080410+Muna-Saeed@users.noreply.github.com>
2024-08-03 19:06:07 +0300

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,7 +7,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError, NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 class DB:
@@ -67,7 +68,7 @@ class DB:
         except TypeError:
             raise InvalidRequestError("Invalid query arguments.")
 
-    def update_user(self, user_id: int, **kwargs) -> None:
+    def update_user(self, user_id: int, **kwargs: dict[str, any]) -> None:
         """Update a user's attributes
 
         Args:
